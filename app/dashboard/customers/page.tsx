69515f01d763f3a4f2fd91986960dc447a285dc3
Allison Bates <allisonkbates@gmail.com>
2024-05-25 13:02:05 -0400

    Completed Tutorial!
    Wrapped up tutorial including customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+    searchParams,
+    }: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+    }) {
+    const query = searchParams?.query || '';
+    // const currentPage = Number(searchParams?.page) || 1; 
+    const customers = await fetchFilteredCustomers(query);
+    // console.log(customers);  
+
+  return (
+    <div className="w-full">
+       <Suspense key={query} fallback={<TableRowSkeleton />}>
+            <Table customers={customers}/>
+        </Suspense>
+    </div>
+  );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -176,7 +175,10 @@ export async function fetchInvoiceById(id: string) {
   }
 }
 
-export async function fetchCustomers() {
+export async function fetchCustomers(
+  query: string,
+  curentPage: number,
+) {
   noStore();
   try {
     const data = await sql<CustomerField>`
